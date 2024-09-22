5f424065fbc1f69c87cb210ecd1eaf5866c35e74
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-09-22 18:41:57 +0300

    Add keybindings for opening iTerm2, Chrome, and VSCode with HammerSpoon

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,29 @@
+-- Command + Option + T for open iTerm2
+hs.hotkey.bind({"cmd", "alt"}, "T", function()
+  local app = hs.application.find("iTerm")
+  if app then
+      app:activate()
+  else
+      hs.application.launchOrFocus("/Applications/iTerm.app")
+  end
+end)
+
+-- Command + Option + C for open Chrome
+hs.hotkey.bind({"cmd", "alt"}, "C", function()
+  local app = hs.application.find("Google Chrome")
+  if app then
+      app:activate()
+  else
+      hs.application.launchOrFocus("/Applications/Google Chrome.app")
+  end
+end)
+
+-- Command + Option + X for open VSCode
+hs.hotkey.bind({"cmd", "alt"}, "X", function()
+  local app = hs.application.find("Visual Studio Code")
+  if app then
+      app:activate()
+  else
+      hs.application.launchOrFocus("/Applications/Visual Studio Code.app")
+  end
+end)
