3f3c26c7d875e26472c5e94bf8feb246c9db0d56
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-10-17 22:32:33 +0300

    Lint hammerspoon config

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,40 +1,39 @@
 -- Command + Option + T for open iTerm2
-hs.hotkey.bind({"ralt"}, "I", function()
+hs.hotkey.bind({ "ralt" }, "I", function()
   local app = hs.application.find("iTerm")
   if app then
-      app:activate()
+    app:activate()
   else
-      hs.application.launchOrFocus("/Applications/iTerm.app")
+    hs.application.launchOrFocus("/Applications/iTerm.app")
   end
 end)
 
 -- Command + Option + C for open Chrome
-hs.hotkey.bind({"ralt"}, "C", function()
+hs.hotkey.bind({ "ralt" }, "C", function()
   local app = hs.application.find("Zed Browser")
   if app then
-      app:activate()
+    app:activate()
   else
-      hs.application.launchOrFocus("/Applications/Zen Browser.app/")
+    hs.application.launchOrFocus("/Applications/Zen Browser.app/")
   end
 end)
 
 -- Command + Option + X for open VSCode
-hs.hotkey.bind({"ralt"}, "D", function()
+hs.hotkey.bind({ "ralt" }, "D", function()
   local app = hs.application.find("Visual Studio Code")
   if app then
-      app:activate()
+    app:activate()
   else
-      hs.application.launchOrFocus("/Applications/Visual Studio Code.app")
+    hs.application.launchOrFocus("/Applications/Visual Studio Code.app")
   end
 end)
 
 -- Command + Option + X for open Spotify
-hs.hotkey.bind({"ralt"}, "S", function()
+hs.hotkey.bind({ "ralt" }, "S", function()
   local app = hs.application.find("Spotify")
   if app then
-      app:activate()
+    app:activate()
   else
-      hs.application.launchOrFocus("/Applications/Spotify.app")
+    hs.application.launchOrFocus("/Applications/Spotify.app")
   end
 end)
-
